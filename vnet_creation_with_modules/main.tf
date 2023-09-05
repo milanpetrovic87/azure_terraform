7ce37e1873708c82e462cc37990e6c3133393e04
Milan Petrovic Vega <petrovic.milan@vegait.rs>
2023-09-05 16:00:59 +0200

    Devided main.tf from network.tf

--- vnet_creation_with_modules/main.tf
+++ vnet_creation_with_modules/main.tf
@@ -19,43 +19,4 @@ resource "azurerm_resource_group" "vnet_rg" {
   tags     = var.tags
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                     = var.vnet_name
-  location                 = azurerm_resource_group.vnet_rg.location # da li ovde mogu da pozovem var.location? Mozda zato sto smo u variables.tf definisali samo koji je to tip variable...
-  resource_group_name = azurerm_resource_group.vnet_rg.name     ### Zasto ovde ne mogu da pisem samo var.name?
-  address_space       = var.vnet_address_space
-  tags                     = var.tags
-
-}
-
-resource "azurerm_subnet" "subnet" {
-  for_each             = var.subnets
-  name                 = each.value["name"]
-  resource_group_name  = azurerm_resource_group.vnet_rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = each.value["address_prefixes"]
-}
-
-resource "azurerm_public_ip" "bastion_public_ip" {
-  name                = "${var.bastionhost_name}PubIP"
-  resource_group_name = azurerm_resource_group.vnet_rg.name
-  location            = azurerm_resource_group.vnet_rg.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = var.tags
-}
-
-resource "azurerm_bastion_host" "bastion_host" {
-  name                = var.bastionhost_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.vnet_rg.name
-  tags                = var.tags
-
-  ip_configuration {
-    name                 = "bastion_host_conf"
-    subnet_id            = azurerm_subnet.subnet["bastion_subnet"].id
-    public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
-  }
-}
-
 
